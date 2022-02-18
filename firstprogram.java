a76cc4d9682e850cdd3231bfe22171c8fa66708c
Vrushabh Sakharwade <61910730+gurusakharwade@users.noreply.github.com>
2022-02-18 23:36:31 +0530

    Add files via upload

---
+++ firstprogram.java
@@ -0,0 +1,27 @@
+public class firstprogram {
+    public static void main(String[] args) {
+        /**int, float, char, string, long, double */ 
+        //int age = 20;
+        //float averagemarks = 19.345f;
+        //char grade = 'A';
+        //String name = "Guru";
+        //boolean isValid = false;
+        //System.out.println("age");
+
+
+        // +, -, /, *, %, ++, --
+
+        // int firstNumber = 1234;
+        // int secondNumber = 234;
+
+        // double sum = (double)firstNumber % (double)secondNumber;
+        // System.out.println(sum);
+
+        int a = 12;
+        System.out.println(++a); // this statement increments by 1 or either use a = a + 1;
+        System.out.println(a);
+        // a--; // this statement decrements by 1
+        // System.out.println(a);
+
+    }
+}
