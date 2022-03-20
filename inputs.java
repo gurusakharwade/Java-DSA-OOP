4d2adf3fd9e3279d5566a9b5d4502021aeed580b
Vrushabh Sakharwade <61910730+gurusakharwade@users.noreply.github.com>
2022-03-21 01:11:59 +0530

    Add files via upload

---
+++ inputs.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class inputs {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter some inputs :");
+        int rollno = input.nextInt();
+        System.out.println("your roll no is " + rollno);
+    }
+}
