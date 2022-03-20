14bfb04223baee78b9a92487472d772b76f7366d
Vrushabh Sakharwade <61910730+gurusakharwade@users.noreply.github.com>
2022-03-20 18:32:58 +0530

    Add files via upload

---
+++ primitives.java
@@ -0,0 +1,12 @@
+public class primitives {
+    public static void main(String[] args) {
+        int a = 10;
+        char letter = 'r';
+        float marks = 98.67f;
+        double largeDecimalNumbers = 4565456.68547;
+        long largeInterger = 3645762576347865L;
+        boolean check = false;
+        System.out.println(largeDecimalNumbers);
+
+    }
+}
