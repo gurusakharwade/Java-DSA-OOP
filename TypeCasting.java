5e9b3142c7debff503d4bcfa93a1bfbbcd7bea7b
Vrushabh Sakharwade <61910730+gurusakharwade@users.noreply.github.com>
2022-03-21 10:36:20 +0530

    Add files via upload

---
+++ TypeCasting.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class TypeCasting {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        float num = input.nextFloat();   //float is greater than integer
+        System.out.println(num);
+    }
+}
