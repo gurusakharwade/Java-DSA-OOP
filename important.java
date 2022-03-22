6f7cce4fc15da75bc1c28ae18f0a3bd727cc4534
Vrushabh Sakharwade <61910730+gurusakharwade@users.noreply.github.com>
2022-03-22 23:23:07 +0530

    Add files via upload

---
+++ important.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class important {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter something :");
+
+        //String name = input.nextLine();   //it will print whole line
+//        String name = input.next(); //it will print first word from line
+//        System.out.println(name);
+        float marks = input.nextFloat();
+        System.out.println(marks);
+    }
+}
