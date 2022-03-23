4b0a0123e6b6884590bfa99182a582546b23930c
Vrushabh Sakharwade <61910730+gurusakharwade@users.noreply.github.com>
2022-03-23 23:39:08 +0530

    Add files via upload

---
+++ unicode.java
@@ -0,0 +1,5 @@
+public class unicode {
+    public static void main(String[] args) {
+        System.out.println("नमस्ते");  // Java prints unicode (means we can use any type of unicode in java)
+    }
+}
