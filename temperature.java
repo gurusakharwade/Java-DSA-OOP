89a0110fb1f9814956c84739d1f5c64941c7c8a2
Vrushabh Sakharwade <61910730+gurusakharwade@users.noreply.github.com>
2022-03-24 10:24:29 +0530

    Add files via upload

---
+++ temperature.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class temperature {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter temperature in c: ");
+        float tempC = in.nextFloat();
+
+        float tempF = (tempC * 9/5) + 32;
+
+        System.out.println(tempF);
+    }
+}
