f4f4cacae159d0092f40437d1d16780332f37c58
Vrushabh Sakharwade <61910730+gurusakharwade@users.noreply.github.com>
2022-03-26 23:51:34 +0530

    Add files via upload

---
+++ Condition and Loops/loopFirstFile.java
@@ -0,0 +1,16 @@
+public class loopFirstFile {
+    public static void main(String[] args) {
+        //Q: print numbers from 1 to 5
+        /*
+           syntax of for loops:
+
+           for (initialization; condition; increment/decrement) {
+              // body
+            }
+         */
+         // Q: Print number from 1 to 5
+         for (int num = 1; num <= 5; num +=1){
+             System.out.println(num);
+         }
+    }
+}
