a06b327269b30f94d581f475242937fd84d036c7
Vrushabh Sakharwade <61910730+gurusakharwade@users.noreply.github.com>
2022-07-18 16:08:54 +0530

    Add files via upload
    This code is used to check the capacity of an Array.

---
+++ Array/Array2.java
@@ -0,0 +1,8 @@
+public class Array2 {
+    public static void main(String[] args) { //program to check array capacity
+        int[] array = new int[6];
+        int capacity = array.length;
+                System.out.println("The array has a capacity of " + capacity);
+
+    }
+}
